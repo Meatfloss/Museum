53b6ebece9e66084bfd2c9cd9e4df14e2affd38b
Xiaochuan Ma <stream@Xiaochuans-Air.fios-router.home>
2016-10-27 05:18:16 -0400

    Run NPM update

--- config/assets/production.js
+++ config/assets/production.js
@@ -22,7 +22,6 @@ module.exports = {
         'public/lib/angular-resource/angular-resource.min.js',
         'public/lib/angular-ui-router/release/angular-ui-router.min.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
-        'public/lib/lodash/lodash.min.js'
         // endbower
       ]
     },
