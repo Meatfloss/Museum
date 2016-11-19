f56bbe5ca818863bcb78e39bfa91428e8c270b97
Xiaochuan Ma <stream@Xiaochuans-Air.fios-router.home>
2016-11-19 08:14:48 -0500

    change production database

--- config/env/production.js
+++ config/env/production.js
@@ -12,7 +12,7 @@ module.exports = {
   host: process.env.HOST || '0.0.0.0',
   sessionSecret: process.env.SESSION_SECRET || 'NYGiangguan12*',
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/museum',
     options: {
       user: '',
       pass: ''
