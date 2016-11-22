e9a2b7ddcc5a2bd58739c704c52c3fb0f581c1d3
Xiaochuan Ma <stream@Xiaochuans-MacBook-Air.local>
2016-11-22 09:33:07 -0500

    Add carouse for home page

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -7,5 +7,24 @@
 
   function HomeController() {
     var vm = this;
+
+    vm.myInterval = 5000;
+    vm.noWrapSlides = false;
+    vm.active = 0;
+    var slides = vm.slides = [];
+    var currIndex = 0;
+
+    vm.addSlide = function (index) {
+      var newWidth = 600 + slides.length + 1;
+      slides.push({
+        image: 'modules/core/client/img/home-slide' + index + '.jpg',
+        text: ['Nice image', 'Awesome photograph', 'That is so cool', 'I love that', 'number 5'][slides.length % 5],
+        id: currIndex++
+      });
+    };
+
+    for (var i = 0; i < 5; i++) {
+      vm.addSlide(i + 1);
+    }
   }
-}());
+} ());
