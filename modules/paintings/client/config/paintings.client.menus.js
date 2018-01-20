d2c196dd9220f41fb20dd99c96dc40206cf0592d
meatfloss <stream@Xiaochuans-Air.fios-router.home>
2018-01-20 10:01:55 -0500

    add new category images

--- modules/paintings/client/config/paintings.client.menus.js
+++ modules/paintings/client/config/paintings.client.menus.js
@@ -10,57 +10,78 @@
   function menuConfig(menuService) {
     menuService.addMenuItem('topbar', {
       title: 'Collections',
-      state: 'paintings',
+      state: 'collections',
       type: 'dropdown',
       roles: ['*'],
       position: 3
     });
 
     // Add the dropdown list item
-    menuService.addSubMenuItem('topbar', 'paintings', {
+    menuService.addSubMenuItem('topbar', 'collections', {
       title: 'Paintings',
       state: 'paintings.landing',
       roles: ['*']
     });
 
     // Add the dropdown list item
-    menuService.addSubMenuItem('topbar', 'paintings', {
+    menuService.addSubMenuItem('topbar', 'collections', {
       title: 'Bronzes',
       state: 'bronzes.list',
       roles: ['*']
     });
 
     // Add the dropdown list item
-    menuService.addSubMenuItem('topbar', 'paintings', {
+    menuService.addSubMenuItem('topbar', 'collections', {
       title: 'Zisha Teapots',
       state: 'teapots.list',
       roles: ['*']
     });
 
     // Add the dropdown list item
-    menuService.addSubMenuItem('topbar', 'paintings', {
+    menuService.addSubMenuItem('topbar', 'collections', {
       title: 'Famille Rose',
       state: 'familles.list',
       roles: ['*']
     });
 
     // Add the dropdown list item
-    menuService.addSubMenuItem('topbar', 'paintings', {
+    menuService.addSubMenuItem('topbar', 'collections', {
       title: 'Famille Rose Enamel',
       state: 'enamels.list',
       roles: ['*']
     });
 
     // Add the dropdown list item
-    menuService.addSubMenuItem('topbar', 'paintings', {
-      title: 'Statues',
+    menuService.addSubMenuItem('topbar', 'collections', {
+      title: 'Ancient Ceramics',
+      state: 'ceramics.list',
+      roles: ['*']
+    });
+
+    // Add the dropdown list item
+    menuService.addSubMenuItem('topbar', 'collections', {
+      title: 'Chai Yao Porcelain',
       state: 'paintings.list',
       roles: ['*']
     });
 
     // Add the dropdown list item
-    menuService.addSubMenuItem('topbar', 'paintings', {
-      title: 'Others',
+    menuService.addSubMenuItem('topbar', 'collections', {
+      title: 'Yuan Dynasty Porcelain',
+      state: 'paintings.list',
+      roles: ['*']
+    });
+
+    // Add the dropdown list item
+    menuService.addSubMenuItem('topbar', 'collections', {
+      title: 'Ming Dynasty Porcelain',
+      state: 'paintings.list',
+      roles: ['*']
+    });
+
+    // Add the dropdown list item
+    menuService.addSubMenuItem('topbar', 'collections', {
+      title: 'Qing Dynasty Porcelain',
       state: 'paintings.list',
       roles: ['*']
     });
