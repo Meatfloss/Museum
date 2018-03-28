a9e94c4f18d303b62f8d0b489fd52f0737f36cb4
ZhangJiawei <Huawen@ZhangJiaweisAir.fios-router.home>
2018-03-27 23:20:02 -0400

    delete

--- modules/yporcelains/client/yporcelains.client.module.js
+++
@@ -1,9 +0,0 @@
-(function (app) {
-  'use strict';
-
-  app.registerModule('yporcelains', ['core']);// The core module is required for special route handling; see /core/client/config/core.client.routes
-  app.registerModule('yporcelains.admin', ['core.admin']);
-  app.registerModule('yporcelains.admin.routes', ['core.admin.routes']);
-  app.registerModule('yporcelains.services');
-  app.registerModule('yporcelains.routes', ['ui.router', 'core.routes', 'articles.services']);
-}(ApplicationConfiguration));
